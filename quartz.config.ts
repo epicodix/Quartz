7cc12fed0412b49f34c50a768c6d8556fe2b11dc
epicodix <irixoen@gmail.com>
2025-11-03 15:56:48 +0900

    Update content: 2025-11-03 15:56

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "epicodix.github.io/Quartz",
     ignorePatterns: ["private", "templates", ".obsidian", "99_임시"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
